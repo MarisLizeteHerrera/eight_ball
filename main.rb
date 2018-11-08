8c0030f5398fa18b6a8f02ae6f4fa67ec1b4853b
ian <clarksware24@gmail.com>
2018-11-08 15:07:17 -0700

    structure Start class

--- main.rb
+++ main.rb
@@ -9,10 +9,30 @@ class Start
   # - User inputs "QUIT"
   # - Computer outputs a goodbye message and exits
   # - This class manages the other classes
+  def initialize()
+    puts "MAGIC 8 BALL\nplease enter a question"
+    question = gets.chomp.to_s
+    case question
+    when "add_answers"
+      Add_answers.new
+    when "reset_answers"
+      Reset_answers.new
+    when  "print_answers"
+      Print_answers.new
+    when  "quit"
+      puts "Goodbye"
+    else 
+      basic
+    end
+  end
+
 end
 
 class Add_answers
   include Answers
+  def initialize ()
+    add_answers
+  end
   # - ability to add more answers:
   # - via easter egg question ("add_answers")
   # - do not let them add the same answer if the eight ball already has that answer
