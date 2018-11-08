e6abc0b279d8cf3346ce0f7476b87a44b0ff8c2d
Maris Herrera <maris.lizete.herrera@gmail.com>
2018-11-08 14:56:37 -0700

    outline skeleton

--- answers.rb
+++ answers.rb
@@ -1,2 +1,29 @@
 
-module 
+#method that creates a list of answers
+#method that takes user input to create new answers
+#method that resets the answers back to default
+#method that prints all the answers on the screen
+
+@answerarray = []
+
+module Answers
+  def basic
+    #default yes no answers
+  end
+
+  def add_answers
+    #add new answers
+    #redirects to start
+  end
+
+  def reset_answers
+    #resets answers
+    #redirects to start
+  end
+
+  def print_answers
+    #prints all answers
+    #redirects to start
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,30 +1,33 @@
-# Basic Objectives:
 
-# - User inputs question
-# - Computer outputs random answer
-# - User inputs "QUIT"
-# - Computer outputs a goodbye message and exits
-# - ability to add more answers:
-# - via easter egg question ("add_answers")
-# - do not let them add the same answer if the eight ball already has that answer
-# - ability to reset answers back to the original bank (hint: think arr.clone)
-# - via easter egg question ("reset_answers")
-# - ability to have eight ball print all answers
-# - via easter egg question ("print_answers")
+require_relative "./answers"
 
 
-class Answers
-
+class Start
+  include Answers
+  # - User inputs question
+  # - Computer outputs random answer
+  # - User inputs "QUIT"
+  # - Computer outputs a goodbye message and exits
+  # - This class manages the other classes
 end
 
 class Add_answers
-
-end
+  include Answers
+  # - ability to add more answers:
+  # - via easter egg question ("add_answers")
+  # - do not let them add the same answer if the eight ball already has that answer
+  end
 
 class Reset_answers
-
+  include Answers
+  # - ability to reset answers back to the original bank (hint: think arr.clone)
+  # - via easter egg question ("reset_answers")
 end
 
 class Print_answers
-
+  include Answers
+  # - ability to have eight ball print all answers
+  # - via easter egg question ("print_answers")
 end
+
+Start.new
